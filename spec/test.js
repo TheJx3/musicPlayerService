d6ab8a1d67a6077dce880d40b5636b66a6cbead4
ringo_sanchez <ringo196@gmail.com>
2018-07-20 19:34:56 -0700

    Add test skeleton

---
+++ spec/test.js
@@ -0,0 +1,27 @@
+import puppeteer from 'puppeteer';
+
+const pageUrl = 'http://localhost:6969/';
+
+let page;
+let browser;
+const width = 1280;
+const height = 720;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+afterAll(() => {
+  browser.close();
+});
+
+describe('MusicPlayer component', () => {
+  test('trial run', () => {
+
+  });  
+});
