5385a16e1770bde3aa99f3fa53ee1343cdbc0165
ringo_sanchez <ringo196@gmail.com>
2018-07-25 01:41:46 -0700

    Test run for circleci  without jest specs

--- spec/test.js
+++ spec/test.js
@@ -1,28 +1,28 @@
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 
-const pageUrl = 'http://localhost:6969/';
+// const pageUrl = 'http://localhost:6969/';
 
-let page;
-let browser;
-const width = 1280;
-const height = 720;
+// let page;
+// let browser;
+// const width = 1280;
+// const height = 720;
 
-beforeAll(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 80,
-    args: [`--window-size=${width},${height}`]
-  });
-  page = await browser.newPage();
-  await page.setViewport({ width, height });
-});
+// beforeAll(async () => {
+//   browser = await puppeteer.launch({
+//     headless: false,
+//     slowMo: 80,
+//     args: [`--window-size=${width},${height}`]
+//   });
+//   page = await browser.newPage();
+//   await page.setViewport({ width, height });
+// });
 
-afterAll(() => {
-  browser.close();
-});
+// afterAll(() => {
+//   browser.close();
+// });
 
-describe('MusicPlayer component', () => {
-  test('trial run', () => {
+// describe('MusicPlayer component', () => {
+//   test('trial run', () => {
 
-  });
-});
+//   });
+// });
