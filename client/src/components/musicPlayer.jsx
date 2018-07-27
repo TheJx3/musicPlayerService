03144f149e73d98b5c717448964959829990b206
ringo_sanchez <ringo196@gmail.com>
2018-07-27 03:01:12 -0700

    Setup most of css for general video player look

--- client/src/components/musicPlayer.jsx
+++ client/src/components/musicPlayer.jsx
@@ -1,7 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import songs from '../../../sampleData.js';
+import styled from 'styled-components';
+// import songs from '../../../sampleData.js';
+
+const MusicPlayerContainer = styled.div`
+  display: flex;
+  flex-direction: row; 
+  background: linear-gradient(135deg, #4D586E, #3A67C0);
+  height: 340px;
+  width: 1200px;
+  margin: 0 auto;
+  color: white;
+  padding-top: 15px;
+  padding-left: 25px;
+  padding-bottom: 15px;
+  padding-right: 15px;
+  justify-content: space-between;
+`;
+
+const ButtonContainer = styled.div`
+  padding-top: 20px;
+
+`;
+
+const ButtonTitleContainer = styled.div`
+padding-top: 20px;
+  display: flex;
+  flex-direction: row; 
+`;
+
+const TitleArtistContainer = styled.div`
+  padding-left: 40px;
+  display: flex;
+  flex-direction: column; 
+  width: 400px;
+  font-weight: 100;
+`;
+
+const AlbumArtContainer = styled.div`
+  width: 340px;
+  height: 340px;
+  align-self: center;
+`;
+
+const playButton = (<span className="fa-stack">
+    <i className="fa fa-circle fa-stack-1x circle-bg"></i>
+    <i className="fa fa-play-circle fa-stack-1x icon-b"></i>
+</span>);
+
+const Artist = styled.span`
+  background-color: black;
+  font-size: 16px;
+  color: #ccc;
+    padding-top: 2px;
+    padding-right: 7px;
+    padding-bottom: 3px;
+    padding-left: 7px;
+`;
+
+const Album = styled.span`
+  background-color: black;
+  font-size: 16px;
+  color: #ccc;
+    line-height: 1;
+    padding-top: 2px;
+    padding-right: 7px;
+    padding-bottom: 3px;
+    padding-left: 7px;
+`;
+
+const AlbumArt = styled.img`
+
+`;
+
+const Title = styled.span`
+  background-color: black;
+  font-size: 24px;
+  color: white;
+  line-height: 2.1;
+    padding-top: 4px;
+    padding-right: 7px;
+    padding-bottom: 4px;
+    padding-left: 7px;
+`;
+
+
 
 class MusicPlayer extends React.Component {
   constructor (props) {
@@ -12,13 +96,9 @@ class MusicPlayer extends React.Component {
         "title": "reprehenderit marfa quinoa bag",
         "artist": "Rick Astley",
         "genre": "Jazz",
-       "album": "Whenever You Need Sombeody"
+        "album": "Whenever You Need Sombeody"
       },
       currentSongId: 9,
-      // currentSongTitle: null,
-      // currentSongArtist: null, 
-      // currentSongGenre: null,
-      // currentSongAlbum: null,
     };
   }
 
@@ -31,7 +111,7 @@ class MusicPlayer extends React.Component {
     $.get(`/api/songs/${songId}`, null, (data) => {
       this.setState({ currentSongData: data });
       document.title = this.state.currentSongData.title;
-      console.log(this.state.currentSongData);
+      // console.log(this.state.currentSongData);
     });
   }
 
@@ -62,20 +142,37 @@ class MusicPlayer extends React.Component {
 
   render () {
     return (
-      <div>
-        <div>
-          Title: {this.state.currentSongData.title}
-        </div>
-        <div>
-          Artist: {this.state.currentSongData.artist}
-        </div>
-        <div>
-          Album: {this.state.currentSongData.album}
-        </div>
+      <MusicPlayerContainer>
+        <ButtonTitleContainer>
+          <ButtonContainer>
+            {playButton}
+          </ButtonContainer>
+          <TitleArtistContainer>
+            <div>
+              <Artist>
+                {this.state.currentSongData.artist}
+              </Artist>
+            </div>
+            <div>
+              <Title>
+                {this.state.currentSongData.title}
+              </Title>
+            </div>
+            <div>
+              <Album>
+                In album: {this.state.currentSongData.album}
+              </Album>
+            </div>
+          </TitleArtistContainer>
+        </ButtonTitleContainer>
+
         <div>
           Genre: {this.state.currentSongData.genre}
         </div>
-      </div>
+        <AlbumArtContainer>
+          <AlbumArt src='http://via.placeholder.com/340x340'></AlbumArt>
+        </AlbumArtContainer>
+      </MusicPlayerContainer>
     );
   }
 }
