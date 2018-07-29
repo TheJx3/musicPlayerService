c04a713cdcdf5e5a4c62a5f240bed15bbf61fb4b
ringo_sanchez <ringo196@gmail.com>
2018-07-29 10:55:21 -0700

    Add modal feature upon clicking album art

---
+++ client/src/components/albumModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import styled from 'styled-components';
+
+const albumModal = document.getElementById('albumModal')
+
+class AlbumModal extends React.Component {
+  constructor (props) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount () {
+    albumModal.appendChild(this.el);
+  }
+
+  componentWillUnmount () {
+    albumModal.removeChild(this.el);
+  }
+
+  render () {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el
+    );
+  }
+}
+
+export default AlbumModal;
