7d7e4140fdd4b257532f9962b5ff2e95a4f1023d
ringo_sanchez <ringo196@gmail.com>
2018-07-30 11:39:10 -0700

    Add basic test for shallow render

--- spec/test.js
+++ spec/test.js
@@ -1,3 +1,32 @@
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
+import sinon from 'sinon';
+
+import MusicPlayer from '../client/src/components/musicPlayer.jsx';
+import AlbumModal from '../client/src/components/albumModal.jsx';
+
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('renders correctly', () => {
+  const wrapper = shallow(
+    <MusicPlayer />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const wrapper = shallow(
+    <AlbumModal />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
 // import puppeteer from 'puppeteer';
 
 // const pageUrl = 'http://localhost:6969/';
@@ -20,9 +49,3 @@
 // afterAll(() => {
 //   browser.close();
 // });
-
-describe('MusicPlayer component', () => {
-  test('trial run', () => {
-
-  });
-});
