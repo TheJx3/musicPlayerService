93770035a7f49cd5706f2544e34cc2a73bfb54c9
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-08-17 18:51:02 -0700

    Added server paths

--- server/index.js
+++ server/index.js
@@ -2,29 +2,68 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const db = require('../database/index.js');
-
+// const db = require('../database/index.js');
 const app = express();
+const port = 7111;
 
-const port = 6969;
-
+const cassandraDB = require('../database/cassandra.js');
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/songs/:songId', (req, res) => {
-  console.log('this is the req.params.songId: ', req.params.songId);
-  db.getSong(null, req.params.songId, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-
-      res.send(data);
-    }
-  });
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
 });
+
+app.get('/api/songs/:id', (req, res) => {
+  let selectedTrackId = req.params.id;
+  const query = 'SELECT * FROM songs WHERE id=?';
+  // console.log('db client',cassandraDB.client);
+  cassandraDB.client.execute(query, [ selectedTrackId ], { prepare : true })
+  .then(result => res.send(result.rows[0]))
+    .catch(err => res.status(404).send({msg:err}));
+});
+
+const idGenerator = () => {
+  let id = 10000000;
+  return function() {
+    return ++id;
+  }
+}
+
+let generateID = idGenerator();
+
+app.post('/api/songs/', (req, res) => {
+  // id,title,artist,genre,album,albumArt,songFile,createdAt
+
+  let song = [
+    generateID(),
+    req.body.title,
+    req.body.artist,
+    req.body.genre,
+    req.body.album,
+    req.body.albumart,
+    req.body.songfile,
+    req.body.createdat
+  ]
+  console.log('request received');
+  const query = 'INSERT INTO songs(id,title,artist,genre,album,albumArt,songFile,createdAt) VALUES (?,?,?,?,?,?,?,?)';
+  cassandraDB.client.execute(query, song, { prepare : true })
+    .then(() => res.send('POST request to db'))
+    .catch(err => res.status(500).send({msg:err}));
+});
+
+app.put('/api/songs/:id/:prop', (req, res) => {
+  let selectedTrackId = req.params.id;
+  let propToUpdate = req.params.prop;
+  let value = req.body.value;
+  let query = `UPDATE soundofcloud.songs SET ${propToUpdate} = ? WHERE id = ?`
+  console.log(typeof propToUpdate);
+  cassandraDB.client.execute(query, [ value, selectedTrackId ], { prepare : true })
+  .then(() => res.send('POST request to db'))
+  .catch(err => res.status(500).send({msg:err}));
+})
 
 app.listen(port, () => console.log(`Your server has connected and is listening on port: ${port}!!`));
