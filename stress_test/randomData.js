1c169c7ec2ba1f0743ce3bc6189955239a9b1eef
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-08-21 16:28:35 -0700

    artillery tests updated

--- stress_test/randomData.js
+++ stress_test/randomData.js
@@ -12,7 +12,7 @@ function generateRandomData(userContext, events, done) {
   const artist = `${Faker.name.firstName()} ${Faker.name.lastName()}`;
   const title = `${Faker.lorem.word()} ${Faker.lorem.word()}`;
   const url = Faker.image.imageUrl();
-  const songId = Faker.random.number({min:1, max:10000000});
+  const songId = Faker.random.number({min:9800000, max:10000000});
   // add variables to virtual user's context:
   userContext.vars.artist = artist;
   userContext.vars.title = title;
